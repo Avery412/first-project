7d98b0bce1df9394cc2d89491ba76596893578dd
Мартынова <ashiyaelric@gmail.com>
2026-07-08 00:45:34 +0300

    Modify header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import Header from './components/Header' 
-import MainPage from './components/MainPage'    
-import About from './components/About'    
+import Header from './components/Header'
+import MainPage from './components/MainPage'
+import About from './components/About'
 import Contacts from './components/Contacts'
 import Footer from './components/Footer'
 
