c342b3d84657e672546f2cb8686d410cd719806c
Мартынова <ashiyaelric@gmail.com>
2026-07-10 16:48:43 +0300

    Modify Contacts + Footer (tablet and desktop)

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,8 +5,8 @@ function Footer() {
       <div className="w-full h-px bg-[#E5E7EB]"></div>
 
       {/* Footer content */}
-      <div className="flex items-center justify-center h-[64px] px-1">
-        <p className="text-base font-normal text-[#696969] leading-[150%]">
+      <div className="flex items-center h-[64px] px-1">
+        <p className="text-base font-normal text-[#696969] leading-[150%] md:px-[70px]">
           © 2026. ООО "Крым Диджитал Групп"
         </p>
       </div>
